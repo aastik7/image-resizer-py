79ab62fffea72efeb661a49a0882a6f4d4373d8d
Aastik <aastikdude7@gmail.com>
2024-01-17 09:40:37 +0530

    Added error and exception handling

--- resizer.py
+++ resizer.py
@@ -1,16 +1,24 @@
 from PIL import Image
 
 def resize_image(width, height ):
-    image = Image.open('cat.png')
+    try:
+        image = Image.open('cat.png')
+    except FileNotFoundError:
+        print("Error : No image was found")
 
 
     print ( f"Original Size: {image.size}" )
+    try:
+        resized_image = image.resize((width,height))
+        resized_image.save(f'resized-image_{width}x{height}.png' )
+        print("Image was resized sucessfully")
+    except Exception as e:
+        print(f"Error during Resizing and saving: {e}")
 
-    resized_image = image.resize((width,height))
-
-    resized_image.save(f'resized-image_{width}x{height}.png' )
-
-width = int(input("Enter Width: "))
-height = int(input("Enter Height: "))
-resize_image(width,height)
-print(f"Resized Image Size: {width}x{height}")
+try:
+    width = int(input("Enter Width: "))
+    height = int(input("Enter Height: "))
+    resize_image(width,height)
+    print(f"Resized Image Size: {width}x{height}")
+except ValueError:
+    print("Error: Please enter inegers for width and height")
